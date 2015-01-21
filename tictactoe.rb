b7d5df1a2cacebedd94fd5f9260bdce1ddaf22b0
Adam Hardy <ahardy55@gmail.com>
2015-01-21 16:24:41 -0600

    add methods to format board and print board

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,7 +1,33 @@
-module TicTacToe
+# module TicTacToe
 
   class Board
-    @board = Array.new(3) {Array.new(3)}
+    def initialize
+      @board = Array.new(3) {Array.new(3)}
+    end
+
+    def format_board
+      num = 0
+      @board.each do |row|
+        row.map! do |space|
+          space = num
+          num += 1
+        end
+      end
+    end
+
+    def print_board
+      self.format_board
+      @board.each do |row|
+        row.each do |space|
+          puts space
+        end
+      end
+    end
+
+
   end
-end
+# end
+
+board = Board.new
+board.print_board
 
