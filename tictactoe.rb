96d37dba8306ae227b06539f83ebbdc7bc60a915
Adam Hardy <ahardy55@gmail.com>
2015-01-21 16:56:17 -0600

    create method to map move

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,13 +1,14 @@
 # module TicTacToe
 
   class Board
+    attr_accessor :game_board
     def initialize
-      @board = Array.new(3) {Array.new(3)}
+      @game_board = Array.new(3) {Array.new(3)}
     end
 
     def format_board
       num = 0
-      @board.each do |row|
+      @game_board.each do |row|
         row.map! do |space|
           space = num
           num += 1
@@ -16,8 +17,7 @@
     end
 
     def print_board
-      self.format_board
-      @board.each do |row|
+      @game_board.each do |row|
         puts row.each { |space| space}.join(" ")
       end
     end
@@ -26,6 +26,17 @@
   class Game
     def initialize(board = Board.new)
       @board = board
+      @board.format_board
+      @board.print_board
+    end
+
+    def map_move(coordinate)
+      @coordinate = coordinate
+
+      if @coordinate == "1"
+        @board.game_board[0][0] = "X"
+      end
+
       @board.print_board
     end
 
@@ -33,6 +44,7 @@
 # end
 
 board = Game.new
+board.map_move("1")
 
 
 
